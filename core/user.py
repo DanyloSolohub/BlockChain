3c455cbf35cfbabd80fdac0f1a79b23162f95d12
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2022-07-17 22:44:01 +0300

    added sign method to user

--- core/user.py
+++ core/user.py
@@ -1,6 +1,6 @@
 from Crypto.PublicKey import RSA
 from dataclasses import dataclass, field
-
+from hashlib import sha512
 from Crypto.PublicKey.RSA import RsaKey
 
 from main import KEY_SIZE
@@ -19,3 +19,13 @@ class User:
         public_key = self.private_key.publickey()
         encoded_key = base64.b64encode(public_key.export_key())
         return encoded_key.decode('utf-8')
+
+    def sign(self, message_to_sign: str) -> str:
+        hash_ = int.from_bytes(sha512(message_to_sign.encode('utf-8')).digest(), byteorder='big')
+        signature = pow(hash_, self.private_key.d, self.private_key.n)
+        return hex(signature)
+
+    def verify_signature(self, message_to_verify: str, signature: str) -> bool:
+        hash_ = int.from_bytes(sha512(message_to_verify.encode('utf-8')).digest(), byteorder='big')
+        hash_from_signature = pow(int(signature, 16), self.private_key.e, self.private_key.n)
+        return hash_ == hash_from_signature
